bbe868bb3823ee683035acb80de9c58a1ee4f248
Dominique Guerrero <domguerr@hotmail.com>
2024-10-11 15:52:48 -0600

    practice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,23 @@ import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import * as sessionActions from './store/session';
 
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+
 const router = createBrowserRouter([
   {
     path: '/',
