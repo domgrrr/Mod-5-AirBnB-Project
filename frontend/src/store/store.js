af5d1fd864473f96637957f59a49b1187195f146
Dominique Guerrero <domguerr@hotmail.com>
2024-10-11 11:49:30 -0600

    Session actions and reducer

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';  //  Import the session reducer
 
 const rootReducer = combineReducers({
+  session: sessionReducer,  //  Add the session reducer
   // ADD REDUCERS HERE
 });
 
