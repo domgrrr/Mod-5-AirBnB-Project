595bd0116b547387bc51fa638d130abe317ab409
Dominique Guerrero <domguerr@hotmail.com>
2024-10-14 09:45:51 -0600

    Phase 3

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import ProfileButton from './ProfileButton';
+import ProfileButton from './ProfileButton/ProfileButton';
 import * as sessionActions from '../../store/session';
 
 function Navigation({ isLoaded }) {
