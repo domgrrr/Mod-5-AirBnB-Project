a8f0465cafd17b884bbe1a042a02f50f83c4c082
Dominique Guerrero <domguerr@hotmail.com>
2024-10-14 12:57:40 -0600

    Phase 4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-import * as sessionActions from './store/session';  //  Import session actions from session.js
+import * as sessionActions from './store/session';
+import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ModalProvider>
       <Provider store={store}>
         <App />
+        <Modal />
       </Provider>
     </ModalProvider>
   </React.StrictMode>
