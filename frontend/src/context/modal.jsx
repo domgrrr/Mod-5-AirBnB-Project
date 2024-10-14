8d3370a3eb5b08b681bc587d02c6af6700bdcbde
Dominique Guerrero <domguerr@hotmail.com>
2024-10-14 12:48:07 -0600

    Phase 4

---
+++ frontend/src/context/modal.jsx
@@ -0,0 +1,25 @@
+import { useRef, createContext } from 'react';
+
+const ModalContext = createContext();
+
+export function ModalProvider({ children }) {
+  const modalRef = useRef();
+  const [modalContent, setModalContent] = useState(null);
+  const [onModalClose, setOnModalClose] = useState(null);
+
+  const contextValue = {
+    modalRef, // reference to modal div
+    modalContent, // React component to render inside modal
+    setModalContent, // function to set the React component to render inside modal
+    setOnModalClose // function to set the callback function to be called when modal is closing
+  };
+
+  return (
+    <>
+      <ModalContext.Provider value={contextValue}>
+        {children}
+      </ModalContext.Provider>
+      <div ref={modalRef} />
+    </>
+  );
+}
