88a47219715446ee1e8e61a120caf173771b5462
Dominique Guerrero <domguerr@hotmail.com>
2024-10-15 10:56:58 -0600

    Expectations and GoogleMapAPI

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
+const mapsRouter = require('/maps');
 
+router.use('/maps', mapsRouter);
 router.use('/api', apiRouter);
 
 // Static routes
