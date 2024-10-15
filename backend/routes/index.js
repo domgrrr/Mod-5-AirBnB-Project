472140a81a8e3203aa37651f19f095a4bc48b089
Dominique Guerrero <domguerr@hotmail.com>
2024-10-15 11:08:59 -0600

    Revert "Expectations and GoogleMapAPI"
    This reverts commit 88a47219715446ee1e8e61a120caf173771b5462.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
-const mapsRouter = require('/maps');
 
-router.use('/maps', mapsRouter);
 router.use('/api', apiRouter);
 
 // Static routes
